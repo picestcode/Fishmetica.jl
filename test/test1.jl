36657278778c22017dc50c353c6db86968db4117
picestcode <picestcode@yandex.ru>
2018-08-14 17:00:03 +1000

    Update test1.jl
    paths to files deleted

--- test/test1.jl
+++ test/test1.jl
@@ -1,13 +1,13 @@
 #test1
-F=readdlm("/home/igor/.julia/v0.6/Fishmetica/test/F.txt")
+F=readdlm("F.txt")
 
-M=readdlm("/home/igor/.julia/v0.6/Fishmetica/test/M.txt")
+M=readdlm("M.txt")
 
-W=readdlm("/home/igor/.julia/v0.6/Fishmetica/test/W.txt")
+W=readdlm("W.txt")
 
-x=readdlm("/home/igor/.julia/v0.6/Fishmetica/test/x.txt")
+x=readdlm("x.txt")
 
-y=readdlm("/home/igor/.julia/v0.6/Fishmetica/test/y.txt")
+y=readdlm("y.txt")
 
 Fishmetica.testgmx(x,M,F)
 Fishmetica.testgmy(x,y,M,F,W,1,1)
